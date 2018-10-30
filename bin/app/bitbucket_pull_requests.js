e077540c718181705dc603a31711f895c7881a64
davidblj <david.blj95@gmail.com>
2018-10-30 09:13:42 -0500

    agregar input del usuario/repo

--- bin/app/bitbucket_pull_requests.js
+++ bin/app/bitbucket_pull_requests.js
@@ -1,70 +1,51 @@
 (function() {
+
     var splunkjs        = require("splunk-sdk");
     var ModularInputs   = splunkjs.ModularInputs;
     var Logger          = ModularInputs.Logger;
     var Event           = ModularInputs.Event;
     var Scheme          = ModularInputs.Scheme;
     var Argument        = ModularInputs.Argument;
-    // other global variables here
 
-    // getScheme method returns introspection scheme
     exports.getScheme = function() {
+
         var scheme = new Scheme("Bitbucket pull requests");
 
-        // scheme properties
         scheme.description = "Streaming de pull requests";
-        scheme.useExternalValidation = true;  // if true, must define validateInput method
-        scheme.useSingleInstance = true;      // if true, all instances of mod input passed to
-                                              //   a single script instance; if false, user 
-                                              //   can set the interval parameter under "more settings"
+        scheme.useExternalValidation = false;  
+        scheme.useSingleInstance = true;
 
-        // add arguments
         scheme.args = [
             new Argument({
-                name: "arg",
-                dataType: Argument.dataTypeNumber,
-                description: "An argument.",
+                name: "username",
+                dataType: Argument.dataTypeString,
+                description: "El nombre del usuario dueño del repositorio.",
                 requiredOnCreate: true,
                 requiredOnEdit: false
             }),
             new Argument({
-                name: "count",
-                dataType: Argument.dataTypeNumber,
-                description: "A counter.",
+                name: "repository",
+                dataType: Argument.dataTypeString,
+                description: "El nombre del repositorio o repo-slug",
                 requiredOnCreate: true,
                 requiredOnEdit: false
             })
-            // other arguments here
         ];
 
         return scheme;
     };
 
-    // validateInput method validates the script's configuration (optional)
-    exports.validateInput = function(definition, done) {
-        // local variables here
+    /*exports.validateInput = function(definition, done) {
         var count = parseInt(definition.parameters.count, 10);
+        // done() or done(error)
+    };*/
 
-        // error checking goes here
-	    if (count < 0) {
-            done(new Error("The count was a negative number."));
-        }
-        else {
-            done();
-        }
-    };
-
-    // streamEvents streams the events to Splunk Enterprise
     exports.streamEvents = function(name, singleInput, eventWriter, done) {
 
-        Logger.info(name, "success !");
+        let username = singleInput.username;
+        let repository = singleInput.repository;
 
-        // local variables here
-        var arg = parseInt(singleInput.arg);
-        var count = parseInt(singleInput.count, 10);
-        var errorFound = false;
-
-        // streaming is done
+        Logger.info(name, "success !:" + username + "/" + repository);
         done();
     };
 
